c4cec219e4fc29bca82bef5a9fcdfc8833650527
TranPhatAn <123556098+TranPhatAn@users.noreply.github.com>
2023-06-13 18:18:28 +0700

    Update DragonSeaMasterForSteve.lua

--- DragonSeaMasterForSteve.lua
+++ DragonSeaMasterForSteve.lua
@@ -1,6 +1,6 @@
 
 if game.PlaceId == 10464237910 then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/TranPhatAn/AnHub/main/drgsealuachoanhrejicudai2cm.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TranPhatAn/AnHub/main/AutoRaidAndFarmingDragonSea.lua'))()
 end 
 if game.PlaceId == 13373351184 then 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/TranPhatAn/AnHub/main/raid.lua'))()
