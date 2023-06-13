b9ee2e4a6258c02b86882fff5008040d251fe6e7
TranPhatAn <123556098+TranPhatAn@users.noreply.github.com>
2023-06-13 18:17:21 +0700

    Update DragonSeaMasterForSteve.lua

--- DragonSeaMasterForSteve.lua
+++ DragonSeaMasterForSteve.lua
@@ -1,1 +1,7 @@
 
+if game.PlaceId == 10464237910 then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TranPhatAn/AnHub/main/drgsealuachoanhrejicudai2cm.lua'))()
+end 
+if game.PlaceId == 13373351184 then 
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TranPhatAn/AnHub/main/raid.lua'))()
+ end
