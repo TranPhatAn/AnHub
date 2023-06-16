b5a786a457d4382cef6d054b2619b356ed819118
TranPhatAn <123556098+TranPhatAn@users.noreply.github.com>
2023-06-16 21:06:51 +0700

    Update DragonSeaMasterForReiji.lua

--- DragonSeaMasterForReiji.lua
+++ DragonSeaMasterForReiji.lua
@@ -1,6 +1,8 @@
-if game.PlaceId == 10464237910 then
+if game.PlaceId == 10464237910 or game.PlaceId == 13373351184 then 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/TranPhatAn/AnHub/main/drgsealuachoanhrejicudai2cm.lua'))()
 end 
-if game.PlaceId == 13373351184 then 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/TranPhatAn/AnHub/main/raid.lua'))()
+else
+ game.Players.LocalPlayer:Kick("90/112/115/32/72/117/98/32/78/111/116/32/83/117/112/112/111/114/116/32/71/97/109/101")
+    wait(1)
+    game:Shutdown()
 end
